ed64d36d526318ecf69ab9f3e7baf9fc547e3d29
Yerold <112283864+yeroldsan@users.noreply.github.com>
2024-03-14 15:10:10 +0200

    build: Add Coil to manage images, Retrofit and GSON

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,9 @@ android {
 
 dependencies {
 
+    implementation(libs.coil.compose)
+    implementation(libs.retrofit)
+    implementation (libs.converter.gson)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
