a8ee86d09e1aa7d57025af924729666523e2be41
Yerold <112283864+yeroldsan@users.noreply.github.com>
2024-03-15 21:39:13 +0200

    feat: Enable edge-to-edge content

--- app/src/main/java/com/github/yeroldsan/pelitrack/MainActivity.kt
+++ app/src/main/java/com/github/yeroldsan/pelitrack/MainActivity.kt
@@ -3,6 +3,7 @@ package com.github.yeroldsan.pelitrack
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -16,31 +17,16 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            enableEdgeToEdge()
             PelitrackTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    PelitrackApp()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PelitrackTheme {
-        Greeting("Android")
     }
 }
