681e94439fa4b16344eedac2f5a4663e6df46698
Yerold <112283864+yeroldsan@users.noreply.github.com>
2024-03-15 21:36:40 +0200

    feat: Add series data class

--- app/src/main/java/com/github/yeroldsan/pelitrack/model/PelitrackData.kt
+++ app/src/main/java/com/github/yeroldsan/pelitrack/model/PelitrackData.kt
@@ -2,16 +2,28 @@ package com.github.yeroldsan.pelitrack.model
 
 import com.google.gson.annotations.SerializedName
 
-data class PelitrackData(
+data class MoviesResponse(
     @SerializedName("results") val movies: List<Movie>,
 ) {
-
     data class Movie(
         val id: Int,
         val title: String,
         val overview: String,
         @SerializedName("poster_path") val posterPath: String?,
         @SerializedName("release_date") val releaseDate: String,
+        @SerializedName("vote_average") val voteAverage: Double,
+        @SerializedName("vote_count") val voteCount: Int
+    )
+}
+data class SeriesResponse(
+    @SerializedName("results") val series: List<Serie>
+) {
+    data class Serie(
+        val id: Int,
+        val name: String,
+        val overview: String,
+        @SerializedName("poster_path") val posterPath: String?,
+        @SerializedName("first_air_date") val firstAirDate: String,
         @SerializedName("vote_average") val voteAverage: Double,
         @SerializedName("vote_count") val voteCount: Int
     )
