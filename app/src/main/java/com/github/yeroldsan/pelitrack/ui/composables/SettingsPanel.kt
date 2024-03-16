fd0343c6692a93984ce923a03fa64c060ced3251
Yerold <112283864+yeroldsan@users.noreply.github.com>
2024-03-16 18:27:50 +0200

    feat: Create settings in bottom sheet

---
+++ app/src/main/java/com/github/yeroldsan/pelitrack/ui/composables/SettingsPanel.kt
@@ -0,0 +1,93 @@
+package com.github.yeroldsan.pelitrack.ui.composables
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import com.github.yeroldsan.pelitrack.R
+import com.github.yeroldsan.pelitrack.viewmodel.PelitrackViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SettingsPanel(vModel: PelitrackViewModel) {
+    ModalBottomSheet(
+        modifier = Modifier.imePadding(),
+        onDismissRequest = {  vModel.closeSettings() }
+    ) {
+        Column (
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment =  Alignment.CenterHorizontally,
+            modifier = Modifier.padding(24.dp)
+        ) {
+            var keyHidden by remember { mutableStateOf(true) }
+            var userApiKey by remember { mutableStateOf("") }
+//                        val apiKey by vModel.apiKey.collectAsState()
+
+            Text(
+                text = "Save your API key below.",
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.onSurface,
+                modifier = Modifier.padding(bottom = 24.dp)
+            )
+
+            OutlinedTextField(
+                value = userApiKey,
+                onValueChange = { it -> userApiKey = it },
+                label = { Text(text = "API Key") },
+                singleLine = true,
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = Color.Transparent,
+                ),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Password,
+                    imeAction = ImeAction.Done
+                ),
+                visualTransformation = if (keyHidden) PasswordVisualTransformation() else VisualTransformation.None,
+                trailingIcon = {
+                    val visibilityIcon = if (keyHidden) R.drawable.baseline_visibility_24 else R.drawable.baseline_visibility_off_24
+                    // Localized description for accessibility services
+                    val description = if (keyHidden) "Show key" else "Hide key"
+
+                    IconButton(
+                        onClick = { keyHidden = !keyHidden }
+                    ) {
+                        Icon(painterResource(id = visibilityIcon), contentDescription = description)
+                    }
+                }
+            )
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 38.dp, vertical = 24.dp),
+                onClick = {  vModel.setApiKey(userApiKey); vModel.closeSettings() }
+            ) {
+                Text(text = "Save")
+            }
+        }
+    }
+}
