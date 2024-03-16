95fa968003d6f06c043c4246d62321ad32355f7c
Yerold <112283864+yeroldsan@users.noreply.github.com>
2024-03-16 18:32:10 +0200

    ui: Update title

--- app/src/main/java/com/github/yeroldsan/pelitrack/ui/composables/MainTopAppBar.kt
+++ app/src/main/java/com/github/yeroldsan/pelitrack/ui/composables/MainTopAppBar.kt
@@ -9,19 +9,14 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.navigation.NavController
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainTopAppBar(openSettings: () -> Unit, scrollBehavior: TopAppBarScrollBehavior, navController: NavController) {
-    var openBottomSheet by rememberSaveable { mutableStateOf(false) }
     TopAppBar(
-        title = { Text(text = "Pelitrack", maxLines = 1, overflow = TextOverflow.Ellipsis) },
+        title = { Text(text = "Popular", maxLines = 1, overflow = TextOverflow.Ellipsis) },
         scrollBehavior = scrollBehavior,
         actions = {
             IconButton(
