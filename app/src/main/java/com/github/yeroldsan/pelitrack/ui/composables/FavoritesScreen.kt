84299147e44c2d46f11065c2e6fcb87b5ce42b8e
Yerold <112283864+yeroldsan@users.noreply.github.com>
2024-03-16 11:20:03 +0200

    feat: Add favorites shows/ movies screen

---
+++ app/src/main/java/com/github/yeroldsan/pelitrack/ui/composables/FavoritesScreen.kt
@@ -0,0 +1,22 @@
+package com.github.yeroldsan.pelitrack.ui.composables
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+
+@Composable
+fun FavoritesScreen(navController: NavController) {
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Place for your favorites")
+        Text(text = "... coming soon")
+    }
+}
