76909af10d403294c0090198a00f793006582459
Yerold <112283864+yeroldsan@users.noreply.github.com>
2024-03-16 18:24:33 +0200

    feat: Create error screen

---
+++ app/src/main/java/com/github/yeroldsan/pelitrack/ui/screens/ErrorScreen.kt
@@ -0,0 +1,32 @@
+package com.github.yeroldsan.pelitrack.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.github.yeroldsan.pelitrack.R
+
+@Composable
+fun ErrorScreen(retryAction: () -> Unit) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Icon(painter = painterResource(id = R.drawable.baseline_cloud_off_24), contentDescription = "Failed to load")
+        Text(text = "Failed to load", modifier = Modifier.padding(16.dp))
+        Button(
+            onClick = retryAction
+        ) {
+            Text("Retry")
+        }
+    }
+}
