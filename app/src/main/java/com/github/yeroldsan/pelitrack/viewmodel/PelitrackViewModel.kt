6dfc4a206210d0b8038e971c0f910c707010284a
Yerold <112283864+yeroldsan@users.noreply.github.com>
2024-03-16 18:39:06 +0200

    feat(app): Add sealed interface for UI state, refactor mutable state flow
    Add openSettings, set api key, close settings bottom sheet and retry functions used form view modal

--- app/src/main/java/com/github/yeroldsan/pelitrack/viewmodel/PelitrackViewModel.kt
+++ app/src/main/java/com/github/yeroldsan/pelitrack/viewmodel/PelitrackViewModel.kt
@@ -3,33 +3,75 @@ package com.github.yeroldsan.pelitrack.viewmodel
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.github.yeroldsan.pelitrack.model.PelitrackData
+import com.github.yeroldsan.pelitrack.model.MoviesResponse
 import com.github.yeroldsan.pelitrack.model.PelitrackRepository
+import com.github.yeroldsan.pelitrack.model.SeriesResponse
+import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
-class PelitrackViewModel: ViewModel() {
-    private val _movies = MutableStateFlow<List<PelitrackData.Movie>>(emptyList())
+sealed interface PelitrackUiState {
+    data class Success(val movies: List<MoviesResponse.Movie>, val series: List<SeriesResponse.Serie>) : PelitrackUiState
+    object Initial : PelitrackUiState
+    object Loading : PelitrackUiState
+    object Error : PelitrackUiState
+}
 
-    val movies: StateFlow<List<PelitrackData.Movie>> = _movies
+class PelitrackViewModel: ViewModel() {
+    private val _pelitrackUiState = MutableStateFlow<PelitrackUiState>(PelitrackUiState.Loading)
+    val pelitrackUiState: StateFlow<PelitrackUiState> = _pelitrackUiState
+
+    // Settings
+    private val _openBottomSheet = MutableStateFlow(false)
+    val openBottomSheet: StateFlow<Boolean> = _openBottomSheet
+
+    private val _apiKey = MutableStateFlow<String?>(null)
+    val apiKey: StateFlow<String?> = _apiKey
+
+    // Data
+    private val _movies = MutableStateFlow<List<MoviesResponse.Movie>>(emptyList())
+    private val _series = MutableStateFlow<List<SeriesResponse.Serie>>(emptyList())
 
     init {
-        getMovies()
+        if (apiKey.value != null) {
+            fetchMoviesAndSeries()
+        } else {
+            _pelitrackUiState.value = PelitrackUiState.Initial
+        }
     }
 
-    private fun getMovies() {
-        viewModelScope.launch {
-            try {
-                val result = PelitrackRepository.create().getMovies()
-                if (result.isSuccessful) {
-                    val moviesResponse = result.body()
-                    _movies.value = moviesResponse?.movies ?: emptyList()
+    fun openSettings() {
+        _openBottomSheet.value = !_openBottomSheet.value
+    }
+
+    fun setApiKey(userApiKey: String?) {
+        _apiKey.value = userApiKey
+        fetchMoviesAndSeries()
+    }
+
+    fun closeSettings() {
+        _openBottomSheet.value = false
+    }
+
+    fun retryFetch() {
+        fetchMoviesAndSeries()
+    }
+
+    private fun fetchMoviesAndSeries() {
+        try {
+            viewModelScope.launch {
+                val moviesDeferred = viewModelScope.async { PelitrackRepository.create().getMovies(apiKey.value) }
+                val seriesDeferred = viewModelScope.async { PelitrackRepository.create().getSeries(apiKey.value) }
+                val moviesResponse = moviesDeferred.await()
+                val seriesResponse = seriesDeferred.await()
+                if (moviesResponse.isSuccessful && seriesResponse.isSuccessful) {
+                    _pelitrackUiState.value = PelitrackUiState.Success(moviesResponse.body()?.movies ?: emptyList(), seriesResponse.body()?.series ?: emptyList())
                 }
-                Log.d("Result", result.body().toString())
-            } catch (e: Exception) {
-                Log.d("Error", e.toString())
             }
+        } catch (e: Exception) {
+            Log.d("Error", e.toString())
+            _pelitrackUiState.value = PelitrackUiState.Error
         }
     }
 }
