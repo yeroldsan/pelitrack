1bce47d2f0c6c088318b93ea5ffa6720afdb3b5e
Yerold <112283864+yeroldsan@users.noreply.github.com>
2024-03-16 18:22:25 +0200

    chore: Remove unused imports

--- app/src/main/java/com/github/yeroldsan/pelitrack/MainActivity.kt
+++ app/src/main/java/com/github/yeroldsan/pelitrack/MainActivity.kt
@@ -7,10 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.github.yeroldsan.pelitrack.ui.theme.PelitrackTheme
 
 class MainActivity : ComponentActivity() {
